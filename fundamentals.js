0e2ed6ae2847f4f8c8f657a96e67f627ebf93e5d
mamun-prodhan <mamunpr9@gmail.com>
2023-05-29 23:50:10 +0600

    first commmit

---
+++ fundamentals.js
@@ -0,0 +1,19 @@
+const numbers = [89, 35, 98, 12];
+
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    console.log(element);
+}
+
+const superStar = {
+    name: "Sakib Khan",
+    age: 32,
+    movies: ['dhakar king', 'password', 'nawab', 'goloi'],
+    tag: "Super star"
+}
+
+const myVariable = 'movies';
+
+console.log(superStar.name);
+console.log(superStar['tag']);
+console.log(superStar[myVariable]);
